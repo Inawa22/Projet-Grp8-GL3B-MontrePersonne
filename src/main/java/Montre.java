bcc84f10854d28979ee4b1d27a284d7afd81173f
sikatiKenmogne <71256717+Samuel-S-K@users.noreply.github.com>
2022-02-02 20:38:21 +0100

    Nettoyage et réusinage de la classe Montre

--- src/main/java/Montre.java
+++ src/main/java/Montre.java
@@ -1,30 +1,45 @@
 
 public class Montre {
-    int hr, min;
-    Person p;
 
-    Montre(int h, int m) {
+    private int hr, min;
+
+    public Montre(int h, int m) {
         this.hr = h;
         this.min = m;
     }
 
-    Montre(Montre m) {
+    public Montre(Montre m) {
         this.hr = m.hr;
         this.min = m.min;
     }
 
-    void newday() {
-        if ((this.min+1 )>59 && (this.hr+1 )>23 ) {
-            this.hr = 0;
+    public int getHr(){
+        return hr;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setHr(int hr) {
+        this.hr = hr;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void oneMinuteMore(){
+        if ((this.min+1 )>59){
             this.min = 0;
-        }
-        else if((this.min+1 )>59){
-            this.min = 0;
-            this.hr++;
-        }
-        else{
+            if((this.hr+1 )>23){
+                this.hr = 0;
+            }else{
+                this.hr++;
+            }
+        }else{
             this.min++;
         }
-
     }
+
 }
