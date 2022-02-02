4e26a5cde48b679e4cda839f4f30aa8140a2e88b
sikatiKenmogne <71256717+Samuel-S-K@users.noreply.github.com>
2022-02-02 23:59:06 +0100

    Implementation du test de création d'un objet montre

--- src/main/java/MontreTest.java
+++ src/main/java/MontreTest.java
@@ -1,6 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MontreTest {
+    Montre rolex = null;
+
+    @BeforeEach
+    public void init(){
+        rolex = new Montre(9,45);
+    }
 
     @org.junit.jupiter.api.Test
     void oneMinuteMore() {
@@ -8,5 +16,6 @@ class MontreTest {
 
     @org.junit.jupiter.api.Test
     void main() {
+        assertNotNull(rolex);
     }
 }
