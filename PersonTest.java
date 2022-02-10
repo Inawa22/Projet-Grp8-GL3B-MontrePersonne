163e9333e309e9f5e79db9405bdb397e450a2f7c
Brunobell45 <98895157+Brunobell45@users.noreply.github.com>
2022-02-10 16:02:35 +0100

    Add files via upload
    A ce niveau j'ai revu et mis a jour les test unitaires concernant la classe PersonTest,java

---
+++ PersonTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @org.junit.jupiter.api.Test
+    void setWatch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWatch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeWatch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void giveTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void askHour() {
+    }
+}
